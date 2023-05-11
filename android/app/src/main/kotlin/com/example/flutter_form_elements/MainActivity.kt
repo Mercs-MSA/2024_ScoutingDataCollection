dd9c5f180aa3f4ebb7b819576834c923866170e5
Kevin Ahr <meowmeowahr@gmail.com>
2023-05-11 17:16:20 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_form_elements/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_form_elements
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flex_color_picker/flex_color_picker.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+void main() {
+  runApp(const FormElementsApp());
+}
+
+class FormElementsApp extends StatelessWidget {
+  const FormElementsApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.red,
+          brightness: Brightness.light,
+        ),
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.red,
+          brightness: Brightness.dark,
+        ),
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.system,
+      home: const FormAppPage(),
+    );
+  }
+}
+
+class FormAppPage extends StatefulWidget {
+  const FormAppPage({super.key});
+  @override
+  State<FormAppPage> createState() => _FormAppPageState();
+}
+
+class _FormAppPageState extends State<FormAppPage> {
+  bool checkBoxIsChecked = false;
+  bool switchIsToggled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Form Elements'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Text Field',
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Team Number',
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              CheckboxListTile(
+                title: const Text('Checkbox'),
+                value: checkBoxIsChecked,
+                onChanged: (bool? newValue) {
+                  setState(() {
+                    checkBoxIsChecked = newValue!;
+                  });
+                },
+              ),
+              const SizedBox(height: 8.0),
+              SwitchListTile(
+                  title: const Text('Switch'),
+                  value: switchIsToggled,
+                  onChanged: (bool? newValue) {
+                    setState(() {
+                      switchIsToggled = newValue!;
+                    });
+                  }),
+              const ColorInput(
+                title: 'Color Select',
+              ),
+              RatingInput(
+                title: 'Rating',
+                onRatingUpdate: (rating) {
+                  print(rating.toInt());
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RatingInput extends StatelessWidget {
+  const RatingInput({
+    super.key,
+    required this.title,
+    required this.onRatingUpdate,
+  });
+
+  final String title;
+  final Function(double) onRatingUpdate;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      trailing: RatingBar.builder(
+        itemBuilder: (context, _) => const Icon(
+          Icons.star,
+          color: Colors.amber,
+        ),
+        onRatingUpdate: onRatingUpdate,
+        glow: false,
+      ),
+    );
+  }
+}
+
+class ColorInput extends StatefulWidget {
+  const ColorInput({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<ColorInput> createState() => _ColorInputState();
+}
+
+class _ColorInputState extends State<ColorInput> {
+  Color currentColor = Colors.red;
+  Color beforeColor = Colors.red;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.title),
+      trailing: ColorIndicator(
+        onSelect: () async {
+          beforeColor = currentColor;
+          if (!(await colorPickerDialog())) {
+            setState(() {
+              currentColor = beforeColor;
+            });
+          }
+        },
+        color: currentColor,
+        width: 36,
+        height: 36,
+      ),
+    );
+  }
+
+  Future<bool> colorPickerDialog() async {
+    return ColorPicker(
+      color: currentColor,
+      onColorChanged: (Color color) => setState(() => currentColor = color),
+      heading: Text(
+        'Select color',
+        style: Theme.of(context).textTheme.titleSmall,
+      ),
+      subheading: Text(
+        'Select color shade',
+        style: Theme.of(context).textTheme.titleSmall,
+      ),
+      wheelSubheading: Text(
+        'Selected color and its shades',
+        style: Theme.of(context).textTheme.titleSmall,
+      ),
+      showColorName: true,
+      pickersEnabled: const <ColorPickerType, bool>{
+        ColorPickerType.primary: true,
+        ColorPickerType.accent: false,
+        ColorPickerType.wheel: true,
+      },
+    ).showPickerDialog(context);
+  }
+}
