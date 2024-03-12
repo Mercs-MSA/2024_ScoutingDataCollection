daa8498d65d68072899d884bf992b31cde05fd35
Kevin Ahr <meowmeowahr@gmail.com>
2024-03-12 12:57:38 -0500

    Cleanup

--- lib/field_forms.dart
+++ lib/field_forms.dart
@@ -90,221 +90,182 @@ class _FieldAutonFormState extends State<FieldAutonForm> {
                     ),
                   ),
                   const SizedBox(height: 8.0),
-                  SizedBox(
-                    height: 400,
-                    child: Row(
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text("SPEAKER"),
-                            NumberInput(
-                              title: "Speaker Notes",
-                              miniStyle: true,
-                              style: NumberInputStyle.green,
-                              value: widget.speakerNotes,
-                              onValueAdd: () {
-                                setState(() {
-                                  if (widget.speakerNotes < 10) {
-                                    speakerNotes = widget.speakerNotes + 1;
-                                  }
-                                  widget.onSpeakerNotesChanged(speakerNotes);
-                                });
-                              },
-                              onValueSubtract: () {
-                                setState(() {
-                                  if (widget.speakerNotes > 0) {
-                                    speakerNotes = widget.speakerNotes - 1;
-                                  }
-                                  widget.onSpeakerNotesChanged(speakerNotes);
-                                });
-                              },
-                            ),
-                            NumberInput(
-                              title: "Speaker Notes Missed",
-                              miniStyle: true,
-                              style: NumberInputStyle.red,
-                              value: widget.speakerNotesMissed,
-                              onValueAdd: () {
-                                setState(() {
-                                  if (widget.speakerNotesMissed < 10) {
-                                    speakerNotesMissed =
-                                        widget.speakerNotesMissed + 1;
-                                  }
-                                  widget.onSpeakerNotesMissedChanged(
-                                      speakerNotesMissed);
-                                });
-                              },
-                              onValueSubtract: () {
-                                setState(() {
-                                  if (widget.speakerNotesMissed > 0) {
-                                    speakerNotesMissed =
-                                        widget.speakerNotesMissed - 1;
-                                  }
-                                  widget.onSpeakerNotesMissedChanged(
-                                      speakerNotesMissed);
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text("AMP"),
-                            NumberInput(
-                              title: "Amp Notes",
-                              miniStyle: true,
-                              style: NumberInputStyle.green,
-                              value: widget.ampNotes,
-                              onValueAdd: () {
-                                setState(() {
-                                  if (widget.ampNotes < 10) {
-                                    ampNotes = widget.ampNotes + 1;
-                                  }
-                                  widget.onAmpNotesChanged(ampNotes);
-                                });
-                              },
-                              onValueSubtract: () {
-                                setState(() {
-                                  if (widget.ampNotes > 0) {
-                                    ampNotes = widget.ampNotes - 1;
-                                  }
-                                  widget.onAmpNotesChanged(ampNotes);
-                                });
-                              },
-                            ),
-                            NumberInput(
-                              title: "Amp Notes Missed",
-                              miniStyle: true,
-                              style: NumberInputStyle.red,
-                              value: widget.ampNotesMissed,
-                              onValueAdd: () {
-                                setState(() {
-                                  if (widget.ampNotesMissed < 10) {
-                                    ampNotesMissed = widget.ampNotesMissed + 1;
-                                  }
-                                  widget
-                                      .onAmpNotesMissedChanged(ampNotesMissed);
-                                });
-                              },
-                              onValueSubtract: () {
-                                setState(() {
-                                  if (widget.ampNotesMissed > 0) {
-                                    ampNotesMissed = widget.ampNotesMissed - 1;
-                                  }
-                                  widget
-                                      .onAmpNotesMissedChanged(ampNotesMissed);
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                        VerticalDivider(
-                          color: Colors.white,
-                          width: 4,
-                          thickness: 4,
-                        ),
-                        Spacer(),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            RingedCheckbox(value: true, onChanged: (x) {}),
-                            SizedBox(height: 32),
-                            RingedCheckbox(value: true, onChanged: (x) {}),
-                            SizedBox(height: 32),
-                            RingedCheckbox(value: true, onChanged: (x) {}),
-                            SizedBox(height: 48),
-                          ],
-                        ),
-                        Spacer(),
-                        RotatedTriangle(
-                          rotationAngle: -pi / 2, // radians
-                          color: Colors.transparent,
-                          borderColor: Colors.red,
-                          borderWidth: 5.0,
-                        ),
-                        VerticalDivider(
-                          color: Colors.red,
-                          width: 4,
-                          thickness: 4,
-                        ),
-                        Spacer(),
-                        Stack(
-                          alignment: AlignmentDirectional.center,
-                          children: [
-                            VerticalDivider(
-                              color: Colors.white,
-                              width: 4,
-                              thickness: 4,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                RingedCheckbox(value: true, onChanged: (x) {}),
-                                SizedBox(height: 32),
-                                RingedCheckbox(value: true, onChanged: (x) {}),
-                                SizedBox(height: 32),
-                                RingedCheckbox(value: true, onChanged: (x) {}),
-                                SizedBox(height: 32),
-                                RingedCheckbox(value: true, onChanged: (x) {}),
-                                SizedBox(height: 32),
-                                RingedCheckbox(value: true, onChanged: (x) {}),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 8.0),
-                  const SizedBox(height: 16.0),
                   Visibility(
                     visible: widget.autonExists,
-                    child: NumberInput(
-                      title: "Amp Notes",
-                      value: widget.ampNotes,
-                      onValueAdd: () {
-                        setState(() {
-                          if (widget.ampNotes < 10) {
-                            ampNotes = widget.ampNotes + 1;
-                          }
-                          widget.onAmpNotesChanged(ampNotes);
-                        });
-                      },
-                      onValueSubtract: () {
-                        setState(() {
-                          if (widget.ampNotes > 0) {
-                            ampNotes = widget.ampNotes - 1;
-                          }
-                          widget.onAmpNotesChanged(ampNotes);
-                        });
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 8.0),
-                  Visibility(
-                    visible: widget.autonExists,
-                    child: NumberInput(
-                      title: "Amp Notes Missed",
-                      value: widget.ampNotesMissed,
-                      onValueAdd: () {
-                        setState(() {
-                          if (widget.ampNotesMissed < 10) {
-                            ampNotesMissed = widget.ampNotesMissed + 1;
-                          }
-                          widget.onAmpNotesMissedChanged(ampNotesMissed);
-                        });
-                      },
-                      onValueSubtract: () {
-                        setState(() {
-                          if (widget.ampNotesMissed > 0) {
-                            ampNotesMissed = widget.ampNotesMissed - 1;
-                          }
-                          widget.onAmpNotesMissedChanged(ampNotesMissed);
-                        });
-                      },
+                    child: SizedBox(
+                      height: 400,
+                      child: Row(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("SPEAKER"),
+                              NumberInput(
+                                title: "Speaker Notes",
+                                miniStyle: true,
+                                style: NumberInputStyle.green,
+                                value: widget.speakerNotes,
+                                onValueAdd: () {
+                                  setState(() {
+                                    if (widget.speakerNotes < 10) {
+                                      speakerNotes = widget.speakerNotes + 1;
+                                    }
+                                    widget.onSpeakerNotesChanged(speakerNotes);
+                                  });
+                                },
+                                onValueSubtract: () {
+                                  setState(() {
+                                    if (widget.speakerNotes > 0) {
+                                      speakerNotes = widget.speakerNotes - 1;
+                                    }
+                                    widget.onSpeakerNotesChanged(speakerNotes);
+                                  });
+                                },
+                              ),
+                              NumberInput(
+                                title: "Speaker Notes Missed",
+                                miniStyle: true,
+                                style: NumberInputStyle.red,
+                                value: widget.speakerNotesMissed,
+                                onValueAdd: () {
+                                  setState(() {
+                                    if (widget.speakerNotesMissed < 10) {
+                                      speakerNotesMissed =
+                                          widget.speakerNotesMissed + 1;
+                                    }
+                                    widget.onSpeakerNotesMissedChanged(
+                                        speakerNotesMissed);
+                                  });
+                                },
+                                onValueSubtract: () {
+                                  setState(() {
+                                    if (widget.speakerNotesMissed > 0) {
+                                      speakerNotesMissed =
+                                          widget.speakerNotesMissed - 1;
+                                    }
+                                    widget.onSpeakerNotesMissedChanged(
+                                        speakerNotesMissed);
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text("AMP"),
+                              NumberInput(
+                                title: "Amp Notes",
+                                miniStyle: true,
+                                style: NumberInputStyle.green,
+                                value: widget.ampNotes,
+                                onValueAdd: () {
+                                  setState(() {
+                                    if (widget.ampNotes < 10) {
+                                      ampNotes = widget.ampNotes + 1;
+                                    }
+                                    widget.onAmpNotesChanged(ampNotes);
+                                  });
+                                },
+                                onValueSubtract: () {
+                                  setState(() {
+                                    if (widget.ampNotes > 0) {
+                                      ampNotes = widget.ampNotes - 1;
+                                    }
+                                    widget.onAmpNotesChanged(ampNotes);
+                                  });
+                                },
+                              ),
+                              NumberInput(
+                                title: "Amp Notes Missed",
+                                miniStyle: true,
+                                style: NumberInputStyle.red,
+                                value: widget.ampNotesMissed,
+                                onValueAdd: () {
+                                  setState(() {
+                                    if (widget.ampNotesMissed < 10) {
+                                      ampNotesMissed =
+                                          widget.ampNotesMissed + 1;
+                                    }
+                                    widget.onAmpNotesMissedChanged(
+                                        ampNotesMissed);
+                                  });
+                                },
+                                onValueSubtract: () {
+                                  setState(() {
+                                    if (widget.ampNotesMissed > 0) {
+                                      ampNotesMissed =
+                                          widget.ampNotesMissed - 1;
+                                    }
+                                    widget.onAmpNotesMissedChanged(
+                                        ampNotesMissed);
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                          VerticalDivider(
+                            color: Colors.white,
+                            width: 4,
+                            thickness: 4,
+                          ),
+                          Spacer(),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              RingedCheckbox(value: true, onChanged: (x) {}),
+                              SizedBox(height: 32),
+                              RingedCheckbox(value: true, onChanged: (x) {}),
+                              SizedBox(height: 32),
+                              RingedCheckbox(value: true, onChanged: (x) {}),
+                              SizedBox(height: 48),
+                            ],
+                          ),
+                          Spacer(),
+                          RotatedTriangle(
+                            rotationAngle: -pi / 2, // radians
+                            color: Colors.transparent,
+                            borderColor: Colors.red,
+                            borderWidth: 5.0,
+                          ),
+                          VerticalDivider(
+                            color: Colors.red,
+                            width: 4,
+                            thickness: 4,
+                          ),
+                          Spacer(),
+                          Stack(
+                            alignment: AlignmentDirectional.center,
+                            children: [
+                              VerticalDivider(
+                                color: Colors.white,
+                                width: 4,
+                                thickness: 4,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  RingedCheckbox(
+                                      value: true, onChanged: (x) {}),
+                                  SizedBox(height: 32),
+                                  RingedCheckbox(
+                                      value: true, onChanged: (x) {}),
+                                  SizedBox(height: 32),
+                                  RingedCheckbox(
+                                      value: true, onChanged: (x) {}),
+                                  SizedBox(height: 32),
+                                  RingedCheckbox(
+                                      value: true, onChanged: (x) {}),
+                                  SizedBox(height: 32),
+                                  RingedCheckbox(
+                                      value: true, onChanged: (x) {}),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   const SizedBox(height: 8.0),
