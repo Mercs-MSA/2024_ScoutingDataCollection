e4404f1a42492be5e03bfd1bcdbd5d8efd3083b8
meowmeowahr <meowmeowahr@gmail.com>
2025-01-04 19:18:04 -0600

    remove match scouting support

--- lib/pit_form.dart
+++ lib/pit_form.dart
@@ -195,10 +195,10 @@ class _PitFormState extends State<PitForm> {
   String autonStrat = "";
   String autonRouteDescription = "";
 
-  final MaterialStateProperty<Icon?> thumbIcon =
-      MaterialStateProperty.resolveWith<Icon?>(
-    (Set<MaterialState> states) {
-      if (states.contains(MaterialState.selected)) {
+  final WidgetStateProperty<Icon?> thumbIcon =
+      WidgetStateProperty.resolveWith<Icon?>(
+    (Set<WidgetState> states) {
+      if (states.contains(WidgetState.selected)) {
         return const Icon(Icons.check);
       }
       return const Icon(Icons.close);
