28f93ff8926cead2c2bd5ee8d88799d01e06e4b4
meowmeowahr <meowmeowahr@gmail.com>
2025-01-31 21:07:59 -0600

    working tabs

--- lib/main.dart
+++ lib/main.dart
@@ -876,24 +876,16 @@ class _FormAppPageState extends State<FormAppPage> {
                   else
                     const SizedBox(),
                   if (matchPageIndex == 1)
-                    ListView(
-                      children: [
-                        Column(
-                          children: [
-                            MatchForm(
-                              teamNumberPresent:
-                                  (matchTeamNumber == null ? false : true) &&
-                                      !(matchScouter == ""),
-                              formData: matchScoutingData,
-                              onDataChanged: (data) {
-                                data.forEach((k, v) {
-                                  matchScoutingData[k] = v;
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
+                    MatchForm(
+                      teamNumberPresent:
+                          (matchTeamNumber == null ? false : true) &&
+                              !(matchScouter == ""),
+                      formData: matchScoutingData,
+                      onDataChanged: (data) {
+                        data.forEach((k, v) {
+                          matchScoutingData[k] = v;
+                        });
+                      },
                     )
                   else
                     const SizedBox(),
