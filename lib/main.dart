a5cf336a27495454a3e4e95f571e81a56ba20151
meowmeowahr <meowmeowahr@gmail.com>
2025-04-11 16:12:03 -0500

    dart fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
-import 'dart:ui';
 import 'package:collection/collection.dart';
 
 import 'package:csv/csv.dart';
